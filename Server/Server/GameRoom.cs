0639f9b86a9f04a3ed48896b6f8c8b872cbc34e4
yongjun <yes1312@naver.com>
2021-06-14 15:02:00 +0900

    Unity #3

--- Server/Server/GameRoom.cs
+++ Server/Server/GameRoom.cs
@@ -20,31 +20,70 @@ namespace Server
         {
             foreach (ClientSession s in _session)
                 s.Send(_pendingList);
-
-            Console.WriteLine($"Flushed {_pendingList.Count} items");
+            //Console.WriteLine($"Flushed {_pendingList.Count} items");
             _pendingList.Clear();
         }
 
-        public void Broadcast(ClientSession session, string chat)
+        public void Broadcast(ArraySegment<byte> segment)
         {
-            S_Chat packet = new S_Chat();
-            packet.playerId = session.SessionId;
-            packet.chat = $"{chat} I am {packet.playerId}";
-            ArraySegment<byte> segment = packet.Write();
-
             _pendingList.Add(segment);
-            
         }
 
         public void Enter(ClientSession session)
         {
+            // 플레이어 추가
             _session.Add(session);
-                 session.Room = this;
+            session.Room = this;
+
+            // 신규플레이어에게 플레이어 목록 전송
+            S_PlayerList players = new S_PlayerList();
+            foreach(ClientSession s in _session)
+            {
+                players.players.Add(new S_PlayerList.Player()
+                {
+                    isSelf = (s == session),
+                    playerId = s.SessionId,
+                    posX = s.PosX,
+                    posY = s.PosY,
+                    posZ = s.PosZ,
+                });
+            }
+            session.Send(players.Write());
+
+            // 신규플레이어 입장을 모두에게 알림
+            S_BroadcastEnterGame enter = new S_BroadcastEnterGame();
+            enter.playerId = session.SessionId;
+            enter.posX = 0;
+            enter.posY = 0;
+            enter.posZ = 0;
+            Broadcast(enter.Write());
         }
         
         public void Leave(ClientSession session)
         {
+            // 플레이어 제거
             _session.Remove(session);
+
+            // 모두에게 알림
+            S_BroadcastLeaveGame leave = new S_BroadcastLeaveGame();
+            leave.playerId = session.SessionId;
+            Broadcast(leave.Write());
+        }
+
+        public void Move(ClientSession session, C_Move packet)
+        {
+            // 좌표 변경
+            session.PosX = packet.posX;
+            session.PosX = packet.posX;
+            session.PosX = packet.posX;
+
+            // 모두에게 전송
+            S_BroadcastMove move = new S_BroadcastMove();
+            move.playerId = session.SessionId;
+            move.posX = session.PosX;
+            move.posY = session.PosY;
+            move.posZ = session.PosZ;
+            Broadcast(move.Write());
         }
     }
 }
